821d372e4090e867562025f61207d4e471177a0d
thachpham@ubuntu <phamthach2506@gmail.com>
2024-02-03 10:46:19 +0700

    ci: change project to microservice-module

--- order-service/src/main/java/com/microservice/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/microservice/orderservice/controller/OrderController.java
@@ -1,7 +1,5 @@
 package com.microservice.orderservice.controller;
 
-import java.util.concurrent.CompletableFuture;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
